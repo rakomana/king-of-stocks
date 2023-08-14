339de73e8a51161fed7828ee869143e6c6ede9fa
Prince Rakomana <Princer@clc.co.za>
2023-08-14 12:01:06 +0200

    init readme and test

--- tests/Feature/StockControllerTest.php
+++ tests/Feature/StockControllerTest.php
@@ -13,27 +13,92 @@ class StockControllerTest extends TestCase
 
     public function testStoreMethod()
     {
-        // Your test code for the store method
-        // You can simulate requests and assertions here
-        // Example: $this->post(route('stock.store'), ['key' => 'value'])
+        // Simulate a successful HTTP response from the external API
+        $this->mock(Http::class, function ($mock) {
+            $mock->shouldReceive('withoutVerifying')
+                 ->once()
+                 ->andReturnSelf();
+            $mock->shouldReceive('get')
+                 ->once()
+                 ->with('https://financialmodelingprep.com/api/v3/quote-short/AAPL', [
+                     'apikey' => '3607f50af9251f1c1ebaab9b71fdfb74',
+                 ])
+                 ->andReturn($this->successfulResponse());
+        });
+
+        // Simulate the request
+        $response = $this->postJson(route('stock.store'));
+
+        // Assert that the response is successful
+        $response->assertSuccessful();
     }
 
     public function testShowMethod()
     {
-        // Your test code for the show method
-        // You can simulate requests and assertions here
+        // Create a test stock
+        $stock = Stock::factory()->create(['name' => 'AAPL']);
+
+        // Simulate the request with query parameters
+        $response = $this->getJson(route('stock.show', ['name' => 'AAPL']));
+
+        // Assert that the response is successful
+        $response->assertSuccessful();
+
+        // Add more assertions based on your implementation
     }
 
     public function testSendEmailMethod()
     {
-        // Your test code for the sendEmail method
-        // You can simulate sending email and assertions here
+        // Create a test user
+        $user = User::factory()->create();
+
+        // Mock the generateSummary method to return sample summary data
+        $this->mock(StockController::class, function ($mock) {
+            $mock->shouldReceive('generateSummary')
+                ->once()
+                ->andReturn(json_encode([
+                    [
+                        'name' => 'AAPL',
+                        'current_price' => 177.79,
+                        'percentage_increase' => 10,
+                    ],
+                ]));
+        });
+
+        // Mock the Notification facade to prevent actual notifications
+        Notification::fake();
+
+        // Call the sendEmail method
+        $response = $this->getJson(route('stock.sendEmail'));
+
+        // Assert that the response is successful
+        $response->assertSuccessful();
+
+        // Assert that the notification was sent to the user
+        Notification::assertSentTo($user, NotifyUsersOnStockSummary::class);
+
+        // Add more assertions based on your implementation
     }
 
     public function testCleanUpMethod()
     {
-        // Your test code for the cleanUp method
-        // You can simulate cleanup and assertions here
+        // Create a test stock with an old date
+        $oldStock = Stock::factory()->create(['created_at' => Carbon::now()->subDays(61)]);
+
+        // Create a test stock within the cutoff date
+        $recentStock = Stock::factory()->create();
+
+        // Call the cleanUp method
+        $response = $this->getJson(route('stock.cleanUp'));
+
+        // Assert that the response is successful
+        $response->assertSuccessful();
+
+        // Assert that the old stock record has been deleted
+        $this->assertDatabaseMissing('stocks', ['id' => $oldStock->id]);
+
+        // Assert that the recent stock record still exists
+        $this->assertDatabaseHas('stocks', ['id' => $recentStock->id]);
     }
 
     public function testGenerateSummaryMethod()
@@ -41,4 +106,15 @@ class StockControllerTest extends TestCase
         // Your test code for the generateSummary method
         // You can call the method and make assertions on the result
     }
+
+      // Helper method to generate a mock successful response
+      private function successfulResponse()
+      {
+          return new \Illuminate\Http\Client\Response(json_encode([
+              [
+                  'symbol' => 'AAPL',
+                  'price' => 177.79,
+              ],
+          ]), 200);
+      }
 }
